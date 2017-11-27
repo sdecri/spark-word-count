df204162a6c5ded0cac740846e634c169593540e
simone <simone.decristofaro85@gmail.com>
2017-11-27 15:04:38 +0100

    add master and partition specification

--- src/main/java/com/sdc/spark/word_count/WordCount.java
+++ src/main/java/com/sdc/spark/word_count/WordCount.java
@@ -3,6 +3,17 @@
  */
 package com.sdc.spark.word_count;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import scala.Tuple2;
 
 /**
  * @author Simone De Cristofaro
@@ -10,13 +21,39 @@ package com.sdc.spark.word_count;
  */
 public class WordCount {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
-        String target = "output";
-        if(args.length > 0)
-            target = args[0];
-        System.out.println(target);
+        String input = args[0];
+        String output = args[1];
         
+        int cores = 8;
+        
+        SparkConf sc = new SparkConf()
+                .setMaster(String.format("local[%s]", cores))
+                .setAppName("word-count");
+        try(JavaSparkContext jsc = new JavaSparkContext(sc)){
+            
+            JavaPairRDD<String, Integer> wordCountRdd = 
+                    jsc.textFile(input, cores * 2)
+            .flatMap(line -> Arrays.asList(line.trim().split("\\s+")).iterator())
+            .mapToPair(word -> new Tuple2<>(word, 1))
+            .reduceByKey((count1, count2) -> count1 + count2)
+            ;
+            wordCountRdd.repartition(1).saveAsTextFile(output);
+            
+            manualEnd();
+        }
+        
+    }
+
+    /**
+     * @throws IOException
+     */
+    private static void manualEnd() throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+        System.out.print("Please press ENTER botton to close application...");
+        reader.readLine();
     }
     
 }
